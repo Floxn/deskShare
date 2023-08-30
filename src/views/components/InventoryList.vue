523baee567c0f88cbc3605bf9b1562450cbf4882
Florian Krohmer <florian.krohmer@gmail.com>
2023-08-30 21:43:41 +0200

    preset for inventory list view

--- src/views/components/InventoryList.vue
+++ src/views/components/InventoryList.vue
@@ -1,5 +1,16 @@
 <template>
-  <div></div>
+  <!-- TODO Diese Komponente muss noch weiter in weitere Komponenten unterteilt werden-->
+  <fieldset v-for="category in inventoryData" :key="category.id">
+    <legend>{{ category.name }}</legend>
+    <!-- TODO das hier sollte eine eigene Komponente sein-->
+    <template v-for="categoryName in category" :key="categoryName">
+      <div class="inventory-category" v-for="item in categoryName" :key="item.id">
+        <input type="checkbox" v-model="item.name" />
+        <label :for="item.name">{{ item.name }}</label>
+      </div>
+    </template>
+  </fieldset>
+  <label for="inventory"></label>
 </template>
 
 <script>
