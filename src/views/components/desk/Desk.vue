bba2a44fe6e201901ccdb85bdf04415a4714e745
Florian Krohmer <florian.krohmer@gmail.com>
2023-09-13 22:48:58 +0200

    remove some scss

--- src/views/components/desk/Desk.vue
+++ src/views/components/desk/Desk.vue
@@ -87,61 +87,6 @@ export default {
 @import '@/assets/scss/variables';
 @import '@/assets/scss/mixins';
 
-.desks {
-  --_grid-template-columns: 2fr repeat(4, 1fr) repeat(2, 100px);
-  --_gap: $spacer;
-  --_column-title: 1 / 2;
-  --_column-displays: 2 / 3;
-  --_column-dockingstation: 3 / 4;
-  --_column-special-information: 4 / 5;
-  --_column-room-id: 5 / 6;
-  --_column-btn-edit-save: 6 / 7;
-  --_column-btn-delete: 7 / 8;
-
-  @supports (not (grid-template-columns: subgrid)) {
-    display: flex;
-    flex-direction: column;
-  }
-
-  @supports (grid-template-columns: subgrid) {
-    display: grid;
-    grid-template-columns: var(--_grid-template-columns);
-    gap: var(--_gap);
-  }
-
-  &-head {
-    display: grid;
-    grid-template-columns: var(--_grid-template-columns);
-    gap: var(--_gap);
-    align-items: baseline;
-    padding-bottom: calc($spacer * 0.5);
-    margin-bottom: calc($spacer * 2);
-    border-bottom: 1px solid $primary-color;
-    color: $primary-color;
-  }
-
-  &-title {
-    grid-column: var(--_column-title);
-    font-size: $h2-font-size;
-  }
-
-  &-displays {
-    grid-column: var(--_column-displays);
-  }
-
-  &-dockingstation {
-    grid-column: var(--_column-dockingstation);
-  }
-
-  &-room-id {
-    grid-column: var(--_column-room-id);
-  }
-
-  &-special-information {
-    grid-column: var(--_column-special-information);
-  }
-}
-
 .desk {
   @include list-view;
 
